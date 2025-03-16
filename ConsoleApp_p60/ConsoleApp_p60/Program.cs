4acd78e7b46e0d4d9d19223e53e2eacbd80555d2
Park-gyuri <rbfl0403@naver.com>
2025-03-16 18:20:53 +0900

    Add files via upload

---
+++ ConsoleApp_p60/ConsoleApp_p60/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_p60
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            int result;
+            //a = 100;
+            //b = 50;
+
+            string str;
+            Console.Write("첫 번째 숫자: ");
+            str = Console.ReadLine();
+            a = Convert.ToInt32(str);
+
+            Console.Write("두 번째 숫자: ");
+            str = Console.ReadLine();
+            b = Convert.ToInt32(str);
+
+            result = a + b;
+            Console.WriteLine(a + " + " + b + " = " + result);
+            result = a - b;
+            Console.WriteLine(a + " - " + b + " = " + result);
+            result = a * b;
+            Console.WriteLine(a + " * " + b + " = " + result);
+            result = a / b;
+            Console.WriteLine(a + " / " + b + " = " + result);
+        }
+    }
+}

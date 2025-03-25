3798e3cb956ace8f824a9a66b5fbdd3fb22049f0
Park-gyuri <rbfl0403@naver.com>
2025-03-25 13:11:52 +0900

    Add files via upload

---
+++ ConsoleApp118/ConsoleApp118/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp118
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int decData = 12341234;
+            string hexData = "ABCDEF";
+
+            Console.WriteLine(Convert.ToString(decData,10));
+            Console.WriteLine(Convert.ToString(decData, 2));
+            Console.WriteLine(Convert.ToString(decData, 16));
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine(hexData);
+            Console.WriteLine(Convert.ToString(Convert.ToInt32(hexData, 16), 10));
+            Console.WriteLine(Convert.ToString(Convert.ToInt32(hexData, 16), 2));
+        }
+    }
+}

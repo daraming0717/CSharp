7485c5a5828d36e807da46f9f8b7a8f4acd4082b
Park-gyuri <rbfl0403@naver.com>
2025-03-25 13:13:42 +0900

    Add files via upload

---
+++ WindowsFormsApp_p162/WindowsFormsApp_p162/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace WindowsFormsApp_p162
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_draw = new System.Windows.Forms.Button();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_draw
+            // 
+            this.btn_draw.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.btn_draw.Location = new System.Drawing.Point(12, 12);
+            this.btn_draw.Name = "btn_draw";
+            this.btn_draw.Size = new System.Drawing.Size(88, 35);
+            this.btn_draw.TabIndex = 0;
+            this.btn_draw.Text = "그리기";
+            this.btn_draw.UseVisualStyleBackColor = true;
+            this.btn_draw.Click += new System.EventHandler(this.btn_draw_Click);
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.Location = new System.Drawing.Point(12, 54);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(88, 35);
+            this.btn_clear.TabIndex = 1;
+            this.btn_clear.Text = "초기화";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.btn_draw);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_draw;
+        private System.Windows.Forms.Button btn_clear;
+    }
+}
+
---
+++ WindowsFormsApp_p162/WindowsFormsApp_p162/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Nakov.TurtleGraphics;
+
+namespace WindowsFormsApp_p162
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_draw_Click(object sender, EventArgs e)
+        {
+            Turtle.Delay = 100;
+
+            Turtle.Forward(100);
+            Turtle.Rotate(135);
+            Turtle.Forward(100);
+            Turtle.Rotate(135);
+        }
+
+        private void btn_clear_Click(object sender, EventArgs e)
+        {
+            Turtle.Init();
+        }
+    }
+}

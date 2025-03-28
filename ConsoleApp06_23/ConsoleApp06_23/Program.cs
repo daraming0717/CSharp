512196ff4e5dec992c76b5fe09666e0af8225e22
Park-gyuri <rbfl0403@naver.com>
2025-03-28 14:14:12 +0900

    Add files via upload

---
+++ ConsoleApp06_23/ConsoleApp06_23/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp06_23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, k;
+            i = 0;
+            while (i < 9)
+            {
+                if(i < 5)
+                {
+                    k = 0;
+                    while (k < 4 - i)
+                    {
+                        Console.Write(" ");
+                        k++;
+                    }
+                    k = 0;
+                    while (k < (2 * i) + 1)
+                    {
+                        Console.Write('\u2605');
+                        k++;
+                    }
+                }
+                else
+                {
+                    k = 0;
+                    while (k < i - 4)
+                    {
+                        Console.Write(" ");
+                        k++;
+                    }
+                    k = 0;
+                    while (k < (9 - i) * 2 - 1)
+                    {
+                        Console.Write('\u2605');
+                        k++;
+                    }
+                }
+                    i++;
+                Console.WriteLine();
+            }
+        }
+    }
+}

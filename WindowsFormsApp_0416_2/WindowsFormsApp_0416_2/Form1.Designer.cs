8f58d402d0b29d446972d213cb31a1f03930f954
Park-gyuri <rbfl0403@naver.com>
2025-04-18 13:13:45 +0900

    Add files via upload

---
+++ WindowsFormsApp_0416_2/WindowsFormsApp_0416_2/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace WindowsFormsApp_0416_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tb_id = new System.Windows.Forms.TextBox();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.bindingSource2 = new System.Windows.Forms.BindingSource(this.components);
+            this.btn_login = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_pass = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tb_id
+            // 
+            this.tb_id.Location = new System.Drawing.Point(125, 93);
+            this.tb_id.Name = "tb_id";
+            this.tb_id.Size = new System.Drawing.Size(146, 25);
+            this.tb_id.TabIndex = 0;
+            // 
+            // btn_login
+            // 
+            this.btn_login.Location = new System.Drawing.Point(288, 92);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(85, 75);
+            this.btn_login.TabIndex = 1;
+            this.btn_login.Text = "로그인";
+            this.btn_login.UseVisualStyleBackColor = true;
+            this.btn_login.Click += new System.EventHandler(this.btn_login_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "아이디";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "비밀번호";
+            // 
+            // tb_pass
+            // 
+            this.tb_pass.Location = new System.Drawing.Point(125, 142);
+            this.tb_pass.Name = "tb_pass";
+            this.tb_pass.PasswordChar = '*';
+            this.tb_pass.Size = new System.Drawing.Size(146, 25);
+            this.tb_pass.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(482, 453);
+            this.Controls.Add(this.tb_pass);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_login);
+            this.Controls.Add(this.tb_id);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_id;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.BindingSource bindingSource2;
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb_pass;
+    }
+}
+
---
+++ WindowsFormsApp_0416_2/WindowsFormsApp_0416_2/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_0416_2
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+            if (tb_id.Text == "" || tb_pass.Text == "")
+            {
+                MessageBox.Show("아이디와 비밀번호를 입력하세요.");
+            }
+            else
+            {
+                if (tb_id.Text == "gyuri" && tb_pass.Text =="123")
+                {
+                    MessageBox.Show(tb_id.Text + "님 환영합니다. 창을 닫습니다.");
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("아이디와 비밀번호를 다시 확인하세요.");
+                    tb_id.Text = "";
+                    tb_pass.Text = "";
+                    tb_id.Focus();
+                }
+            }
+        }
+    }
+}
